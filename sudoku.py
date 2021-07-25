599ee6b3309e10565929b30384fab36f8a373f3b
whatserface <79013093+whatserface@users.noreply.github.com>
2021-07-25 21:59:48 +0300

    Refactoring
    I've changed the algorithm of generating field, so it's faster. And have done some refactoring in general to the sudoku class.
    Shout out to the guy, whose algorithm I've used: https://github.com/chanshukwong/Sudoku-Generator (but changed it slightly).

--- sudoku.py
+++ sudoku.py
@@ -1,36 +1,45 @@
 from pygame import Rect, draw, init, font, transform, Surface, display, image, key, event, QUIT, MOUSEBUTTONUP, KEYDOWN, mouse, K_BACKSPACE
-from random import randrange, choice, randint
-import time
-from collections import Counter
+from random import randrange, randint, choices, shuffle
+from time import time
+import numpy as np
+
 class Sudoku():
     def __init__(self):
-        self.i = 0
-        self.field = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                               [4, 5, 6, 7, 8, 9, 1, 2, 3],
-                               [7, 8, 9, 1, 2, 3, 4, 5, 6],
-                               [2, 3, 4, 5, 6, 7, 8, 9, 1],
-                               [5, 6, 7, 8, 9, 1, 2, 3, 4],
-                               [8, 9, 1, 2, 3, 4, 5, 6, 7],
-                               [3, 4, 5, 6, 7, 8, 9, 1, 2],
-                               [6, 7, 8, 9, 1, 2, 3, 4, 5],
-                               [9, 1, 2, 3, 4, 5, 6, 7, 8]]  # создания поля
-        self.mix_funcs()
-        self.start_game()
+        self.field = np.array([
+                      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                      [4, 5, 6, 7, 8, 9, 1, 2, 3],
+                      [7, 8, 9, 1, 2, 3, 4, 5, 6],
+                      [2, 3, 4, 5, 6, 7, 8, 9, 1],
+                      [5, 6, 7, 8, 9, 1, 2, 3, 4],
+                      [8, 9, 1, 2, 3, 4, 5, 6, 7],
+                      [3, 4, 5, 6, 7, 8, 9, 1, 2],
+                      [6, 7, 8, 9, 1, 2, 3, 4, 5],
+                      [9, 1, 2, 3, 4, 5, 6, 7, 8]], dtype=int)  # создания поля
+        self.random_numbers = np.arange(81)
+        shuffle(self.random_numbers)
+        self.shuffle_field()
+        self.player_area = self.field.copy()
+        self.test_grid = self.field.copy()
+        self.generate_field(0)
+
     def transpose(self):
-        self.field = list(map(list, zip(*self.field))) # значения столбцов и значения рядов меняются местами
+        self.field = np.array(list((map(list, zip(*self.field))))) # значения столбцов и значения рядов меняются местами
+
     def swapping_rows_in_area(self):
-        rect = randrange(3) # случайный квадрат
+        random_rect = randrange(3)
         row1 = randrange(3) # ряд квадрата
-        index1 = rect*3 + row1
+        index1 = random_rect*3 + row1
         row2 = randrange(3) # ряд для обмена
         while row2 == row1: # на случай, если ряд один и тот же
             row2 = randrange(3)
-        index2 = rect*3 + row2
-        self.field[index1], self.field[index2] = self.field[index2], self.field[index1]
+        index2 = random_rect*3 + row2
+        self.field[[index1, index2]] = self.field[[index2, index1]]
+
     def swapping_small_columns(self): # обмен значений столбцов
         self.transpose()
         self.swapping_rows_in_area()
         self.transpose()
+
     def swapping_big_rows(self): # обмен 3-мя квадратами по рядам
         area1 = randrange(3) # 3 квадрата
         area2 = randrange(3) # другие два квадрата
@@ -38,68 +47,56 @@ class Sudoku():
             area2 = randrange(3)
         for i in range(3):
             index1, index2 = area1*3 + i, area2*3 + i
-            self.field[index1], self.field[index2] = self.field[index2], self.field[index1]
-    def swapping_big_columns(self): # обмен 3-мя квадрата по столбцам
+            self.field[[index1, index2]] = self.field[[index2, index1]]
+
+    def swapping_big_columns(self): # обмен 3-мя квадратами по столбцам
         self.transpose()
         self.swapping_big_rows()
         self.transpose()
-    def mix_funcs(self): # рандомайзер функций
-        methods = dir(self)[-5:]
-        for i in range(randint(5, 20)):
-            method = choice(methods)
-            eval('self.' + method + '()')
-    def start_game(self):
-        self.i += 1
-        print(f"I've done {self.i} iterations to generate puzzle")
-        self.player_area = [[0 for i in range(9)] for i in range(9)] # то что будет видеть игрок
-        elements = []
-        for i in range(randint(30, 35)):
-            row, collumn = randrange(0, 9, randint(1, 3)), randrange(0, 9, randint(1, 3))
-            while (row, collumn) in elements:
-                row, collumn = randint(0, 8), randint(0, 8)
-            elements.append((row, collumn))
-            self.player_area[row][collumn] = self.field[row][collumn]
-        if not self.check_sudoku(): # если решений судоку больше одного - всё заполнить заново
-            self.start_game()
-    def check_sudoku(self):
-        self.grid = self.player_area
-        self.possible_solutions = set()
-        zv = 0
-        def possible(self, r, c, n):  # r - row, c - collumn, n - number
-            if n in self.grid[r]:
-                return False
-            for i in range(9):
-                if n == self.grid[i][c]:
-                    return False
-            r0 = r // 3 * 3
-            c0 = c // 3 * 3
-            for i in range(3):
-                for j in range(3):
-                    if n == self.grid[r0 + i][c0 + j]:
-                        return False
-            return True
-        def solve_sudoku(self):
-            nonlocal zv
-            for i in range(9):
-                for j in range(9):
-                    if self.grid[i][j] == 0:
-                        for n in range(1, 10):
-                            if possible(self, i, j, n):
-                                self.grid[i][j] = n
-                                solve_sudoku(self)
-                                self.grid[i][j] = 0
-                        return
-            self.possible_solutions.add(tuple(map(tuple, self.grid)))
-            if len(self.possible_solutions) > 1:
-                zv += 1
-                return
-        for i in range(20):
-            solve_sudoku(self)
-            self.i += 1
-            if zv >= 1:
-                return False
-            self.grid = self.player_area
-        return True
+
+    def shuffle_field(self):
+        methods = [self.swapping_big_rows, self.swapping_rows_in_area, self.swapping_big_columns, self.swapping_small_columns, self.transpose]
+        shuffled_methods = choices(methods, k=randint(10, 30))
+        for method in shuffled_methods:
+            method()
+
+    def generate_field(self, index):
+        if index >= 81:
+            return
+        row, col = divmod(self.random_numbers[index], 9)
+        if self.player_area[row][col] != 0:
+            self.test_grid = self.player_area.copy()
+            self.test_grid[row][col] = 0
+            solutions = self.amount_of_solutions(0, 0)
+            if solutions == 1:
+                self.player_area[row][col] = 0
+        self.generate_field(index+1)
+
+
+    def possible_values(self, row, col):
+        start_row = row // 3 * 3
+        start_col = col // 3 * 3
+        return {1, 2, 3, 4, 5, 6, 7, 8, 9} - set(self.test_grid[start_row:start_row + 3, start_col:start_col + 3].flat) - set(self.test_grid[row, :]) - set(self.test_grid[:, col])
+
+    def amount_of_solutions(self, number, possible_solutions: int):
+        if possible_solutions > 1:
+            return possible_solutions
+        if number >= 81:
+            possible_solutions += 1
+            return possible_solutions
+        row, col = divmod(number, 9)
+        if self.test_grid[row][col] > 0:
+            possible_solutions = self.amount_of_solutions(number + 1, possible_solutions)
+        else:
+            values = list(self.possible_values(row, col))
+            shuffle(values)
+            for value in values:
+                self.test_grid[row][col] = value
+                possible_solutions = self.amount_of_solutions(number + 1, possible_solutions)
+                self.test_grid[row][col] = 0
+        return possible_solutions
+
+
 def fill_cell(number, row, collumn):
     if number == '0':
         if (row, collumn) in players_indexes:
@@ -112,19 +109,24 @@ def fill_cell(number, row, collumn):
             players_indexes.append((row, collumn))
         else:
             players_numbers[players_indexes.index((row, collumn))] = (text, int(number))
+
 def check():
     global players_mistakes, remained_cells, playing
     players_mistakes = []
-    for i, tup in enumerate(players_numbers): #i = 0, tup = (0, 1)
+    for i, tup in enumerate(players_numbers):
         if grid.field[players_indexes[i][0]][players_indexes[i][1]] != tup[1]:
             red_surface = Surface((51, 51))
             players_mistakes.append((red_surface, players_indexes[i]))
     if len(players_mistakes) == 0 and len(players_numbers) == remained_cells:
         playing = 0
+
 def init_grid():
     global indexes, ints, players_numbers, players_indexes, players_mistakes, row, collumn, show
     screen.blit(generating_sign, (514, 115))
+    print('Generating sudoku, it may take some time')
+    generate_time = time()
     grid.__init__()
+    print(f'Mesh was generated in {round(time() - generate_time, 2)} seconds')
     indexes = []
     ints = []
     players_numbers, players_indexes, players_mistakes = [], [], []
@@ -136,9 +138,13 @@ def init_grid():
                 indexes.append((i, j))
                 ints.append(text)
     show = False
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     init()
+    print('Generating sudoku, it may take some time')
+    generate_time = time()
     grid = Sudoku()
+    print(f'Mesh was generated in {round(time() - generate_time, 2)} seconds')
     indexes = []
     ints = []
     fnt = font.SysFont('calibri', 40)
@@ -156,7 +162,7 @@ if __name__ == '__main__':
     screen = display.set_mode((800, 600))
     display.set_caption('Sudoku')
     grid_coords = (0, 0)
-    grid_pic = transform.scale(image.load('empty_sudoku_field.png'), (501, 499))
+    grid_pic = transform.scale(image.load(r'D:\Soft\PROGRAMMING\python projects\stuff\empty_sudoku_field.png'), (501, 499))
     grid_pic_rect = grid_pic.get_rect()
     chosen_cell = Surface((51, 51))
     check_lable = fnt.render("ПРОВЕРИТЬ", True, orange)
@@ -167,7 +173,7 @@ if __name__ == '__main__':
     start_coords = (538, 500)
     game, playing = 1, 1
     win1, win2, win1_coords, win2_coords = fnt.render('''You've solved''', True, orange), fnt.render('sudoku!', True, orange), (520, 65), (570, 105)
-    generating_sign, show, once, kostil = fnt.render('Generating...', True, blue), False, False, 0
+    generating_sign, show, once, still = fnt.render('Generating...', True, blue), False, False, 0
     numbers = [[7, 59, 114, 171, 225, 279, 337, 389, 444], [7, 60, 114, 170, 223, 277, 335, 387, 442]]  # (Y, X) - (ROW, COLLUMN)координаты вообще, но пусть будут numbers
     passing = 1
     while game:
@@ -179,7 +185,7 @@ if __name__ == '__main__':
             screen.blit(chosen_cell, element)
         for i in players_mistakes:
             if len(players_numbers) != 0:
-                if (row, collumn) == i[1] and passing == 0:
+                if (row, collumn) == i[1] and p2assing == 0:
                     players_mistakes.remove(i)
                     continue
                 i[0].fill(red)
@@ -188,7 +194,8 @@ if __name__ == '__main__':
             screen.blit(ints[i], (numbers[1][indexes[i][1]], numbers[0][indexes[i][0]]))
         for mmmi in range(len(players_numbers)):
             if len(players_numbers) != 0:
-                screen.blit(players_numbers[mmmi][0], (numbers[1][players_indexes[mmmi][1]], numbers[0][players_indexes[mmmi][0]]))
+                screen.blit(players_numbers[mmmi][0],
+                            (numbers[1][players_indexes[mmmi][1]], numbers[0][players_indexes[mmmi][0]]))
         draw.rect(screen, rect_color, check_button)
         draw.rect(screen, blue, start_button)
         screen.blit(check_lable, check_coords)
@@ -198,19 +205,19 @@ if __name__ == '__main__':
             screen.blit(win2, win2_coords)
         if show:
             screen.blit(generating_sign, (514, 115))
-            kostil += 1
-        if kostil > 1:
+            still += 1
+        if still > 1:
             init_grid()
-            kostil = 0
+            still = 0
         for e in event.get():
             if e.type == QUIT:
                 game = 0
-            if e.type == MOUSEBUTTONUP: #если он кликает ЛКМом на ячейку сетки
+            if e.type == MOUSEBUTTONUP:
                 mouse_pos = mouse.get_pos()
-                if playing: # координаты мышки игрока
+                if playing:
                     if grid_pic_rect.collidepoint(mouse_pos) and screen.get_at(mouse_pos)[:3] != black:
                         x0, y0 = mouse_pos
-                        collumn, row = (x0//55), ((y0-1)//55) # определить какая эта ячейка
+                        collumn, row = (x0 // 55), ((y0 - 1) // 55)  # определить какая эта ячейка
                         if (row, collumn) not in indexes and not (row, collumn) == (None, None):
                             element = (numbers[1][collumn], numbers[0][row])
                             passing = 0
@@ -223,7 +230,6 @@ if __name__ == '__main__':
                         passing = 1
                 if start_button.collidepoint(mouse_pos):
                     show = True
-                    #init_grid()
                     passing = 1
             if e.type == KEYDOWN:
                 if passing == 0 and playing and e.key == K_BACKSPACE:
@@ -232,5 +238,6 @@ if __name__ == '__main__':
                     if e.unicode.isdigit():
                         fill_cell(e.unicode, row, collumn)
         display.update()
-        if show and kostil > 0:
-            kostil += 1
+        if show and still > 0:
+            still += 1
+    print('Exit')
